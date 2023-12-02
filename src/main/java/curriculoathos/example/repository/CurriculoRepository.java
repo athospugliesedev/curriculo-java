6c3edbcb27d440e619a161761e0a5b4360e21424
Athos <athospugliesedev@gmail.com>
2023-12-02 00:26:12 -0300

    crud and more

---
+++ src/main/java/curriculoathos/example/repository/CurriculoRepository.java
@@ -0,0 +1,7 @@
+package curriculoathos.example.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import curriculoathos.example.model.Curriculo;
+
+public interface CurriculoRepository extends CrudRepository<Curriculo, Long> {
+}
